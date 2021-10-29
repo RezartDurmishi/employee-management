fbb4d5015e322f4adfbe5d8cd3358f29f68c5b5e
Sharka <klajdisharka1994@gmail.com>
2021-10-29 17:19:38 +0200

    added others methods

--- src/main/java/com/employee/management/controller/EmployeeController.java
+++ src/main/java/com/employee/management/controller/EmployeeController.java
@@ -2,21 +2,18 @@ package com.employee.management.controller;
 
 import com.employee.management.entity.Employee;
 import com.employee.management.service.EmployeeService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+//import java.util.HashMap;
+//import java.util.Map;
 
 import java.util.List;
 
 @RestController
 public class EmployeeController {
 
-    private EmployeeService service;
+    private final EmployeeService service;
 
     public EmployeeController(EmployeeService service) {
         this.service = service;
@@ -28,13 +25,23 @@ public class EmployeeController {
     }
 
     @PostMapping("/create")
-    public Employee addEmployee(@RequestBody Employee employee ){
+    public Employee addEmployee(@RequestBody Employee employee) {
         return service.saveEmployee(employee);
     }
 
-//     public ResponseEntity<?> findAllEmployees() {
-//        return ResponseEntity.ok("ok");
-//     }
-//    Map ...
+    @GetMapping("/employee/{id}")
+    public Employee findEmployeeById(@PathVariable int id) {
+        return service.getEmployeeById(id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteEmployee(@PathVariable int id) {
+        service.deleteEmployee(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        return service.updateEmployee(employee);
+    }
 
 }
